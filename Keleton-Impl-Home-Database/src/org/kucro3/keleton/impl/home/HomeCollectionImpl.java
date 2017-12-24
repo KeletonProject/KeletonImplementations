d373876a41eb3704efda81c2f692a36e6afaad21
Kumonda221 <kumonda@foxmail.com>
2017-12-25 00:30:06 +0800

    增量更新 20171225

--- Keleton-Impl-Home-Database/src/org/kucro3/keleton/impl/home/HomeCollectionImpl.java
+++ Keleton-Impl-Home-Database/src/org/kucro3/keleton/impl/home/HomeCollectionImpl.java
@@ -2,7 +2,6 @@ package org.kucro3.keleton.impl.home;
 
 import org.kucro3.annotation.CaseInsensitive;
 import org.kucro3.keleton.datalayer.api.home.DataHome;
-import org.kucro3.keleton.datalayer.ref.ResilientReferenceGroup;
 import org.kucro3.keleton.datalayer.ref.sponge.PlayerRelatedCache;
 import org.kucro3.keleton.world.home.Home;
 import org.kucro3.keleton.world.home.HomeCollection;
@@ -27,44 +26,42 @@ public class HomeCollectionImpl implements HomeCollection {
 
         final HomeCollectionImpl pthis = this;
 
-        this.homes.setCallbackOnLogin((player, cache) -> {
-            state = false;
-            final UUID uuid = player.getUniqueId();
-            CompletableFuture.<Void>runAsync(() -> {
-                ResilientReferenceGroup<Homes> group;
-
-                if((group = cache.getGroup(uuid)) != null)
-                    if(group.size() != 0)
-                    {
-                        group.setStrong();
-                        return;
-                    }
-                    else;
-                else
-                    cache.addGroup(uuid, group = new ResilientReferenceGroup<>());
-
-                final Homes collection = new Homes();
-
-                try {
-                    service.db.process((conn) -> DataHome.load(conn, tableName, uuid, (data) -> {
-                        HomeImpl home = new HomeImpl(pthis, data);
-                        collection.put(data.getName(), home);
-                    }));
-
-                    group.add(collection);
-                    failures.remove(uuid);
-                } catch (SQLException e) {
-                    failures.put(uuid, new HomeInternalException("SQL Failure on preloading", e));
-                }
-            }).thenAccept((unused) -> state = true);
-        });
+        this.homes.setCallbackOnLogin((player, cache) -> tryLoad(player.getUniqueId(), cache));
 
         this.homes.setCallbackOnLogoff((player, cache) -> cache.setSoft(player.getUniqueId()));
     }
 
-    void lock()
+    public CompletableFuture<Void> preload(UUID uuid)
     {
-        while(!state);
+        return tryLoad(uuid, homes);
+    }
+
+    CompletableFuture<Void> tryLoad(UUID uuid, PlayerRelatedCache<Homes> cache)
+    {
+        return CompletableFuture.<Void>runAsync(() -> tryLoad(uuid, cache));
+    }
+
+    void tryLoad0(UUID uuid, PlayerRelatedCache<Homes> cache)
+    {
+        if(cache.available(uuid))
+        {
+            cache.setStrong(uuid);
+            return;
+        }
+
+        final Homes collection = new Homes();
+
+        try {
+            service.db.process((conn) -> DataHome.load(conn, tableName, uuid, (data) -> {
+                HomeImpl home = new HomeImpl(this, data);
+                collection.put(data.getName(), home);
+            }));
+
+            cache.set(uuid, collection);
+            failures.remove(uuid);
+        } catch (SQLException e) {
+            failures.put(uuid, new HomeInternalException("SQL Failure on preloading", e));
+        }
     }
 
     void check(UUID uuid)
@@ -72,64 +69,90 @@ public class HomeCollectionImpl implements HomeCollection {
         HomeException e;
         if((e = failures.get(uuid)) != null)
             throw e;
-        lock();
     }
 
     @Override
-    public Optional<Home> getHome(UUID uuid, @CaseInsensitive String name, World world) throws HomeException
+    public CompletableFuture<Optional<Home>> getHome(UUID uuid, @CaseInsensitive String name, World world) throws HomeException
     {
+        final String lname = name.toLowerCase();
+
+        if(homes.available(uuid))
+            return CompletableFuture.completedFuture(getHomeDirectly(uuid, lname, world));
+
+        return CompletableFuture.supplyAsync(() -> {
+            tryLoad0(uuid, homes);
+            return getHomeDirectly(uuid, lname, world);
+        });
+    }
+
+    Optional<Home> getHomeDirectly(UUID uuid, @CaseInsensitive String name, World world) throws HomeException
+    {
+        Home home = homes.get(uuid).get(name);
+        if(home != null && (world == null || home.getLocation().getExtent().equals(world)))
+            return Optional.of(home);
         return Optional.empty();
     }
 
     @Override
-    public boolean hasHome(UUID uuid, @CaseInsensitive String name) throws HomeException
+    public CompletableFuture<Optional<Home>> getHome(UUID uuid, @CaseInsensitive String name) throws HomeException
+    {
+        return getHome(uuid, name, null);
+    }
+
+    @Override
+    public CompletableFuture<Boolean> hasHome(UUID uuid, @CaseInsensitive String name) throws HomeException
+    {
+        return hasHome(uuid, name, null);
+    }
+
+    @Override
+    public CompletableFuture<Boolean> hasHome(UUID uuid, @CaseInsensitive String name, World world) throws HomeException
     {
         return false;
     }
 
     @Override
-    public boolean hasHome(UUID uuid, @CaseInsensitive String name, World world) throws HomeException
+    public CompletableFuture<Boolean> deleteHome(UUID uuid, @CaseInsensitive String name, Cause casue) throws HomeException
     {
         return false;
     }
 
     @Override
-    public boolean deleteHome(UUID uuid, @CaseInsensitive String name, Cause casue) throws HomeException
+    public CompletableFuture<Boolean> clearHomes(Cause cause) throws HomeException
     {
         return false;
     }
 
     @Override
-    public boolean clearHomes(Cause cause) throws HomeException
+    public CompletableFuture<Boolean> clearHomes(World world, Cause casue) throws HomeException
     {
         return false;
     }
 
     @Override
-    public boolean clearHomes(World world, Cause casue) throws HomeException
+    public CompletableFuture<Boolean> clearHomes(UUID uuid, Cause casue) throws HomeException
     {
         return false;
     }
 
     @Override
-    public boolean clearHomes(UUID uuid, Cause casue) throws HomeException
+    public CompletableFuture<Map<String, Home>> getHomes(UUID uuid) throws HomeException
     {
-        return false;
+        Homes collection = homes.get(uuid);
+        return CompletableFuture.completedFuture(homes == null ? Collections.emptyMap() : Collections.unmodifiableMap(homes.get(uuid)));
     }
 
     @Override
-    public Collection<Home> getHomes(UUID uuid) throws HomeException
+    public CompletableFuture<Integer> homeCount(UUID uuid) throws HomeException
     {
         return null;
     }
 
     @Override
-    public Home setHome(UUID uuid, String s, Location<World> location, Cause casue) throws HomeException
+    public CompletableFuture<Home> setHome(UUID uuid, String name, Location<World> location, Cause casue) throws HomeException
     {
         return null;
     }
-
-    volatile boolean state;
 
     private final String tableName;
 
